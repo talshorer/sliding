640615dc5deeb77142ff741e90f5394320675903
Tal Shorer <tal.shorer@gmail.com>
2017-04-30 22:24:49 +0300

    tests: add a test for a nonmatching response
    Signed-off-by: Tal Shorer <tal.shorer@gmail.com>

---
+++ tests/test_nonmatching_response.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+import sliding
+
+import helper
+
+
+class TestNonmatchingResponse(helper.TestCase):
+
+    class Protocol(helper.Protocol):
+
+        def recv(self, state, timeout):
+            resp, cookie = super(TestNonmatchingResponse.Protocol, self).recv(
+                state, timeout)
+            if cookie == 3:
+                cookie = -1
+            return resp, cookie
+
+    def test_nonmatching_response(self):
+        protocol = self.Protocol()
+        offs = range(20)
+        with self.assertRaises(sliding.NonmatchingResponse):
+            sliding.run_sliding_window(protocol, None, 10, 1, iter(offs), 4)
